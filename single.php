835e25d6611dd98e968e6bc96980903dc62cf009
Kajmak101 <Adnan_s@live.dk>
2023-02-22 14:05:10 +0100

    SAVE THAT SHIT

--- single.php
+++ single.php
@@ -4,6 +4,7 @@
  * 
  */
 get_header( );
+echo get_post_format();
 ?>
 
 <div id="primary" class="content-area">
@@ -11,14 +12,20 @@ get_header( );
 <?php
 while(have_posts(  )) :
     the_post( );
-    the_title( '<h2>','</h2>');
-    the_content( );
+    get_template_part( 'template-parts/post/content', get_post_format() );
+    // the_title( '<h2>','</h2>');
+    //the_content( );
+
+    if ( comments_open(  ) || get_comments_number () ):
+        comments_template(); 
+    endif;
 
 endwhile;  
 
 ?>
 
 </main>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
 </div>
 
